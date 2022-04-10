976034e518b246580baf6860663f3366658b9a0c
richard483 <richard.william483@gmail.com>
2022-04-10 21:59:05 +0700

    Penambahan fitur create

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -1,6 +1,8 @@
 package Controller;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
 import Model.Book;
@@ -28,7 +30,7 @@ public class Main {
 			switch (input) {
 			case 1:
 				//input data baru
-				System.out.println("Hello Input!");
+				printMenuInputDataBaru();
 				break;
 			case 2:
 				//view data
@@ -91,5 +93,201 @@ public class Main {
 	public static void main(String[] args) {
 		new Main();
 	}
+	
+	//fitur create
+	public void printMenuInputDataBaru() {
+		int inpNum;
+		do {
+			System.out.println("1. Staff");
+			System.out.println("2. Customer");
+			System.out.println("3. Book");
+			System.out.println("4. Movie");
+			System.out.println("5. Exit");
+			System.out.print("Masukkan pilihan kamu >>> ");
+			inpNum = scanInt();
+			switch (inpNum) {
+			case 1:
+				addStaffData();
+				break;
+			case 2:
+				addCustomerData();
+				break;
+			case 3:
+				addBookData();
+				break;
+			case 4:
+				addMovieData();
+				break;
+			case 0:
+				
+				break;
 
+			default:
+				break;
+			}
+		} while (inpNum != 5);
+	}
+
+
+	public void addStaffData() {
+		String inpNama;
+		String inpPhoneNumber;
+		String inpEmailAddress;
+		String inpGender;
+		String inpStaffId;
+		Integer inpSalary; 
+		do {
+			System.out.println("Masukkan nama staff [length > 1 && < 50]: ");
+			inpNama = scan.nextLine();
+		} while (inpNama.length()< 1 || inpNama.length() >50);
+		do {
+			System.out.println("Masukkan nomor telepon staff [length < 12]: ");
+			inpPhoneNumber = scan.nextLine();
+		} while (inpPhoneNumber.length() >= 12);
+		do {
+			System.out.println("Masukkan Email Staff [must contain '@']: ");
+			inpEmailAddress = scan.nextLine();
+		} while (!(inpEmailAddress.contains("@")));
+		do {
+			System.out.println("Masukkan Gender Staff ['Male' or 'Female']: ");
+			inpGender = scan.nextLine();
+		} while (!(inpGender.equalsIgnoreCase("Male") || inpGender.equalsIgnoreCase("Female")));
+		do {
+			System.out.println("Masukkan Staff ID [must contain EMP]: ");
+			inpStaffId = scan.nextLine();
+		} while (!(inpStaffId.contains("EMP")));
+		do {
+			System.out.println("Masukkan Salary Staff: ");
+			inpSalary = scan.nextInt();
+		} while (!(inpSalary != 0));
+		Staff dataStaff = new Staff(inpNama, inpPhoneNumber, inpEmailAddress, inpGender, inpStaffId, inpSalary);
+		staffList.add(dataStaff);
+	}
+
+	public void addCustomerData() {
+		String inpNama;
+		String inpPhoneNumber;
+		String inpEmailAddress;
+		String inpGender;
+		String inpCustomerId;
+		do {
+			System.out.println("Masukkan nama customer [length > 1 && < 50]: ");
+			inpNama = scan.nextLine();
+		} while (inpNama.length()< 1 || inpNama.length() >50);
+		do {
+			System.out.println("Masukkan nomor telepon customer [length < 12]: ");
+			inpPhoneNumber = scan.nextLine();
+		} while (inpPhoneNumber.length() >= 12);
+		do {
+			System.out.println("Masukkan Email customer [must contain '@']: ");
+			inpEmailAddress = scan.nextLine();
+		} while (!(inpEmailAddress.contains("@")));
+		do {
+			System.out.println("Masukkan Gender customer ['Male' or 'Female']: ");
+			inpGender = scan.nextLine();
+		} while (!(inpGender.equalsIgnoreCase("Male") || inpGender.equalsIgnoreCase("Female")));
+		do {
+			System.out.println("Masukkan customer ID [must contain 'CUS']: ");
+			inpCustomerId = scan.nextLine();
+		} while (!(inpCustomerId.contains("CUS")));
+		Customer customerData = new Customer(inpNama, inpPhoneNumber, inpEmailAddress, inpGender, inpCustomerId);
+		customerList.add(customerData);
+	}
+
+	public void addBookData() {
+		String inpitemID;
+		String inptitle;
+		String inpauthor;
+		String inpgenre;
+		String inpreleaseDate;
+		String inpisbn;
+		String intpublisher;
+		do {
+			System.out.println("Masukkan Item ID [must contain 'BOK']: ");
+			inpitemID = scan.nextLine();
+		} while (!(inpitemID.contains("BOK")));
+		do {
+			System.out.println("Masukkan Book Title: ");
+			inptitle = scan.nextLine();
+		} while (inptitle.length()< 1 || inptitle.length() >50);
+		do {
+			System.out.println("Masukkan Book Author: ");
+			inpauthor = scan.nextLine();
+		} while (inpauthor.length()< 1 || inpauthor.length() >50);
+		do {
+			System.out.println("Masukkan Book Genre: ");
+			inpgenre = scan.nextLine();
+		} while (inpgenre.length()< 1 || inpgenre.length() >50);
+
+		//ini input date gw bikin gada validasi dl (ricat)
+		System.out.println("Masukkan Release Date (dd-mm-yyyy): ");
+		inpreleaseDate = scan.nextLine();
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-mm-yyyy");
+		Date dateTemp = null;
+		try{
+			dateTemp = simpleDateFormat.parse(inpreleaseDate);
+		} catch (Exception e){
+			System.err.println(e);
+		}
+
+		do {
+			System.out.println("Masukkan Book ISBN: ");
+			inpisbn = scan.nextLine();
+		} while (inpisbn.length() >= 12);
+		do {
+			System.out.println("Masukkan Book Publisher: ");
+			intpublisher = scan.nextLine();
+		} while (intpublisher.length()< 1 || intpublisher.length() >50);
+		Book addBookData = new Book(inpitemID, inptitle, inpauthor, inpgenre, dateTemp, inpisbn, intpublisher);
+		bookList.add(addBookData);
+		
+	}
+
+	public void addMovieData() {
+		String inpitemID;
+		String inptitle;
+		String inpauthor;
+		String inpgenre; 
+		String inpreleaseDate; 
+		String inpstudio; 
+		
+		do {
+			System.out.println("Masukkan Item ID: ");
+			inpitemID = scan.nextLine();
+		} while (!(inpitemID.contains("MOV")));
+		do {
+			System.out.println("Masukkan Movie Title: ");
+			inptitle = scan.nextLine();
+		} while (inptitle.length()< 1 || inptitle.length() >50);
+		do {
+			System.out.println("Masukkan Movie Author: ");
+			inpauthor = scan.nextLine();
+		} while (inpauthor.length()< 1 || inpauthor.length() >50);
+		do {
+			System.out.println("Masukkan Movie Genre: ");
+			inpgenre = scan.nextLine();
+		} while (inpgenre.length()< 1 || inpgenre.length() >50);
+
+		//ini juga gw tiadain do-whilenya
+
+		System.out.println("Masukkan Release Date (dd-mm-yyyy): ");
+		inpreleaseDate = scan.nextLine();
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-mm-yyyy");
+		Date dateTemp = null;
+		try{
+			dateTemp = simpleDateFormat.parse(inpreleaseDate);
+		} catch (Exception e){
+			System.err.println(e);
+		}
+		// do {
+		// 	System.out.println("Masukkan Release Date: ");
+		// 	inpreleaseDate = scan.next();
+		// } while (!(inpreleaseDate.contains("January")));
+		do {
+			System.out.println("Masukkan Movie Studio: ");
+			inpstudio = scan.nextLine();
+		} while (inpstudio.length()< 1 || inpstudio.length() >50);
+		Movie addMovieData = new Movie(inpitemID, inptitle, inpauthor, inpgenre, dateTemp, inpstudio);
+		movieList.add(addMovieData);
+	}
 }
